1c616271495e6c3d4c77e327c6f901065e30e9ff
sisi.kylin <fluoritekylin@163.com>
2025-10-27 23:38:23 +0800

    refactor: type issue

--- src/languages/ts/versionSort.ts
+++ src/languages/ts/versionSort.ts
@@ -1,26 +1,25 @@
 export function versionSort(input: string[]):string[] {
-    enum PreRelease {
-        alpha=0,
-        beta=1,
-        rc=2,
+    const PreRelease:Record<string, number> = {
+        alpha:0,
+        beta:1,
+        rc:2,
     }
-    const compareArrayFn = (a: string[], b: string[]) => {
+    const compareArrayFn = (a: string, b: string) => {
         const [coreA, preOrderA] = a.split('-', 2)
         const [coreB, preOrderB] = b.split('-', 2)
-        console.log('-----pre order', preOrderA, preOrderB)
-        const arrA = coreA.split('.')
-        const arrB = coreB.split('.')
+        const arrA = coreA?.split('.') ?? []
+        const arrB = coreB?.split('.') ?? []
         const len = Math.max(arrA.length, arrB.length)
-        for (let i=0;i<len;i++) {
+        for (let i = 0; i < len; i++) {
             const numA = Number(arrA[i] ?? 0)
             const numB = Number(arrB[i] ?? 0)
-            if (numA - numB !==0) return numA - numB
+            if (numA - numB !== 0) return numA - numB
         }
         if (!preOrderA && !preOrderB) return 0
         if (!preOrderA) return 1
         if (!preOrderB) return -1
-        return PreRelease[preOrderA] - PreRelease[preOrderB]
+        return (PreRelease?.[preOrderA] ?? 3) - (PreRelease?.[preOrderB] ?? 3)
     };
 
-    return  input.sort((va, vb) => compareArrayFn(va,vb))
+    return  [...input].sort((va, vb) => compareArrayFn(va,vb))
 }
