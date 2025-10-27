944e41f298e3852a8f223e8d8c6ba7ae8be2cd3a
sisi.kylin <fluoritekylin@163.com>
2025-10-27 18:28:47 +0800

    feat: handle different length

--- src/languages/ts/versionSort.ts
+++ src/languages/ts/versionSort.ts
@@ -1,19 +1,15 @@
 export function versionSort(input: string[]):string[] {
-    const splits = input.map((version) => version
-            .split('.')
-            .map(num => parseInt(num)));
-    const getCompareFn = (a, b) => {
-        if (a[0] - b[0] !== 0) {
-            return a[0] - b[0]
-        } else {
-            if (a[1] - b[1] !== 0) {
-                return a[1] - b[1]
-            } else {
-                return a[2] - b[2]
-            }
+    const splits = input.map((version) => version.split('.'));
+    const compareArrayFn = (a, b) => {
+        const len = Math.max(a.length, b.length)
+        for (let i=0;i<len;i++) {
+            const numA = Number(a[i] ?? 0)
+            const numB = Number(b[i] ?? 0)
+            if (numA - numB !==0) return numA - numB
         }
+        return 0
     };
 
-    splits.sort(getCompareFn)
+    splits.sort(compareArrayFn)
     return splits.map((item) => item.join('.'))
 }
