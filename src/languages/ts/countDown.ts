c477e50c6c28137cc5720e4af2bfdc412f1c82c7
sisi.kylin <fluoritekylin@163.com>
2025-10-28 23:05:43 +0800

    refactor: extract update logic

--- src/languages/ts/countDown.ts
+++ src/languages/ts/countDown.ts
@@ -17,22 +17,27 @@ function mySetInterval(callback: () => void, delay: number): () => void {
 }
 
 function countDown(targetTime: Date) {
-    const now = new Date()
-    const diffSec = Math.floor((targetTime.getTime() - now.getTime())/1000);
-    const stop = mySetInterval(() => {
+    let stop: () => void | undefined
+    const update = () => {
         const now = new Date()
-        const diffSec = Math.floor((targetTime.getTime() - now.getTime())/1000);
-        console.log('now ', now)
-        const hour = Math.floor(diffSec/3600)
-        const min = Math.floor((diffSec%3600)/60)
+        const diffSec = Math.floor((targetTime.getTime() - now.getTime()) / 1000);
+        if (diffSec <= 0) {
+            console.log('Time has come')
+            stop?.()
+            return
+        }
+        const hour = Math.floor(diffSec / 3600)
+        const min = Math.floor((diffSec % 3600) / 60)
         const sec = diffSec % 60
         console.log(`${hour}:${min}:${sec} is remaining`)
-    }, 1000)
-    if (diffSec <= 0) {
-        console.log('Time has come')
-        stop?.()
-        return
-    }
+    };
+
+    update() //first second
+    //start from second
+    setTimeout(() => {
+        stop = mySetInterval(update, 1000)
+    },1000)
+
 }
 
 countDown(new Date('2025-10-28T23:50:00'))
